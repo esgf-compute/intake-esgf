a70345ca9a4565d90ec04d42713a3e5bb3f565e2
Jason Boutte <boutte.jason@gmail.com>
2020-11-13 17:08:56 -0800

    Syncs version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="intake-esgf",
-    version="1.0.2",
+    version="1.0.1",
     packages=find_packages(),
     entry_points={
         "intake.drivers": [
